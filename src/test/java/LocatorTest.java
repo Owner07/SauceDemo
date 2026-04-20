7e6b0e909feac444f93107b65b6d042253e0f894
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-20 23:44:44 +0500

    css selectors

--- src/test/java/LocatorTest.java
+++ src/test/java/LocatorTest.java
@@ -1,12 +1,15 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
 public class LocatorTest {
+    WebDriver driver;
 
     @Test
     public void checkLocator() {
@@ -19,15 +22,37 @@ public class LocatorTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
+        String q = "standard_user";
+        String w = "secret_sauce";
         driver.findElement(By.id("user-name"));
         driver.findElement(By.name("user-name"));
-//        driver.findElement(By.linkText("Logout"));
-//        driver.findElement(By.partialLinkText("Reset"));
-        driver.findElement(By.className("input_error form_input"));
+        driver.findElement(By.className("login_credentials_wrap-inner"));
         driver.findElement(By.tagName("h4"));
-
-        driver.quit();
+        driver.findElement(By.cssSelector(".login_password"));
+        driver.findElement(By.cssSelector(".submit-button.btn_action"));
+        driver.findElement(By.cssSelector(".login_credentials_wrap .login_credentials_wrap-inner"));
+        driver.findElement(By.cssSelector("#login-button"));
+        driver.findElement(By.cssSelector("br"));
+        driver.findElement(By.cssSelector("div.login_credentials"));
+        driver.findElement(By.cssSelector("[data-test='username']"));
+        WebElement user = driver.findElement(By.name("user-name"));
+        user.sendKeys(q);
+        WebElement pass = driver.findElement(By.name("password"));
+        pass.sendKeys(w);
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.cssSelector("[data-test='inventory-item-name']"));
+        driver.findElement(By.cssSelector("[class~='btn']"));
+        driver.findElement(By.cssSelector("[id|='add-to-cart']"));
+        driver.findElement(By.cssSelector("[id^='add-to-cart']"));
+        driver.findElement(By.cssSelector("[id$='backpack']"));
+        driver.findElement(By.cssSelector("[id*='sauce']"));
+    }
+    @AfterMethod
+    public void quit(){
+        if(driver != null){
+            driver.quit();
+        }
     }
 }
